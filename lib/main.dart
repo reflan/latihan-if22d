b995a7c8ea01b4b38500fb102ce7f05e4bf6dd05
reflannuari <reflannuari@gmail.com>
2024-10-10 02:37:22 +0000

    commit tanggal 10 oktober 2024

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:myapp/app/controllers/auth_controller.dart';
 
 import 'app/routes/app_pages.dart';
 import 'firebase_options.dart';
@@ -11,11 +12,26 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final CAuth = Get.put(AuthController(), permanent: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: CAuth.streamAuthStatus,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            return GetMaterialApp(
+              title: 'Mobile II IF 22 C',
+              initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+              getPages: AppPages.routes,
+            );
+          }
+
+          return Text("nanti di sini kita buat proses loading");
+        });
+  }
 }
