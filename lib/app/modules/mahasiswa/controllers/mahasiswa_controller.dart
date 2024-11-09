355c1fe9ebdfcdd0c4e0d47fc0e5f3fa2e123aaf
reflannuari <reflannuari@gmail.com>
2024-11-09 00:28:03 +0000

    commit fitur add

--- lib/app/modules/mahasiswa/controllers/mahasiswa_controller.dart
+++ lib/app/modules/mahasiswa/controllers/mahasiswa_controller.dart
@@ -22,4 +22,51 @@ class MahasiswaController extends GetxController {
 
     return mahasiswa.snapshots();
   }
+
+  void add(String npm, String nama, String alamat) async {
+    CollectionReference mahasiswa = firestore.collection("mahasiswa");
+
+    try {
+      await mahasiswa.add({
+        "npm": npm,
+        "nama": nama,
+        "alamat": alamat,
+      });
+      Get.defaultDialog(
+          title: "Berhasil",
+          middleText: "Berhasil menyimpan data mahasiswa",
+          onConfirm: () {
+            cNpm.clear();
+            cNama.clear();
+            Get.back();
+            Get.back();
+            textConfirm:
+            "OK";
+          });
+    } catch (e) {
+      print(e);
+      Get.defaultDialog(
+        title: "Terjadi Kesalahan",
+        middleText: "Gagal Menambahkan Mahasiswa.",
+      );
+    }
+  }
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    cNpm = TextEditingController();
+    cNama = TextEditingController();
+    cAlamat = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    cNpm.dispose();
+    cNama.dispose();
+    cAlamat.dispose();
+    super.onClose();
+  }
 }
